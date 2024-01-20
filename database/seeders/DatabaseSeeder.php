5d782605f20d4c08671dc0174e10a00526bc6b20
Oskar Lesment <oskar-aleksander.lesment@tptlive.ee>
2024-01-20 19:16:05 +0200

    New seeding works. Seeds couple of posts with category attached

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
 use Illuminate\Database\Seeder;
@@ -14,27 +15,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //\App\Models\User::factory(10)->create();
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
         $user = User::factory()->create();
 
-        Category::create([
+        $personal = Category::create([
             'name' => "Personal",
             'slug' => "personal"
         ]);
         
-        Category::create([
+        $family = Category::create([
             'name' => 'Family',
             'slug' => 'family'
         ]);
         
-        Category::create([
+        $work=Category::create([
             'name' => 'Work',
             'slug' => 'work'
         ]);
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       
+        Post::create([
+            'user_id'=>$user->id,
+            'category_id'=>$family->id,
+            'title'=>'My Family Post',
+            'slug'=> 'my-family-post',
+            'excerpt'=>'Lorem Ipsum',
+            'body'=>'Lorem Ipsum Dolor sit amet'
+        ]);
+
+        Post::create([
+            'user_id'=>$user->id,
+            'category_id'=>$work->id,
+            'title'=>'My work Post',
+            'slug'=> 'my-work-post',
+            'excerpt'=>'Lorem Ipsum',
+            'body'=>'Lorem Ipsum Dolor sit amet'
+        ]);
 
     }
 }
