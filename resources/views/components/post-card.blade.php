de2b8201d7612f6e3d525544ba18e283ab1750b4
Oskar Lesment <oskar-aleksander.lesment@tptlive.ee>
2024-01-22 15:37:28 +0200

    Copied Post featured  tweaks to postcard. Works.

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -6,24 +6,21 @@
 
                         <div class="mt-8 flex flex-col justify-between">
                             <header>
-                                <div class="space-x-2">
-                                    <a href="#"
-                                       class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                                       style="font-size: 10px">Techniques</a>
-                                    <a href="#"
-                                       class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
-                                       style="font-size: 10px">Updates</a>
-                                </div>
+                             <div class="space-x-2">
+                                <a href="/categories/{{ $post->category->slug }}"
+                                   class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
+                                   style="font-size: 10px">{{ $post->category->name }}</a>
+                            </div>
 
-                                <div class="mt-4">
-                                    <h1 class="text-3xl">
-                                        This is a big title and it will look great on two or even three lines. Wooohoo!
-                                    </h1>
+                            <div class="mt-4">
+                                <h1 class="text-3xl">
+                                    {{ $post-> title }}
+                                </h1>
 
-                                    <span class="mt-2 block text-gray-400 text-xs">
-                                        Published <time>1 day ago</time>
+                                <span class="mt-2 block text-gray-400 text-xs">
+                                        Published <time>{{$post->created_at->diffForHumans()}}</time>
                                     </span>
-                                </div>
+                            </div>
                             </header>
 
                             <div class="text-sm mt-4">
